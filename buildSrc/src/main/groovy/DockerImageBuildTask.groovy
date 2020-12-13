f4a6193f9eb95cfb79c080c8474b066b009b420b
kolegran <kolegran@gmail.com>
2020-12-13 20:33:39 +0200

    Added placeholders to the build_docker_image.sh

--- buildSrc/src/main/groovy/DockerImageBuildTask.groovy
+++ buildSrc/src/main/groovy/DockerImageBuildTask.groovy
@@ -1,11 +1,34 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+import java.nio.file.Files
+import java.nio.file.Path
+
 class DockerImageBuildTask extends DefaultTask {
+    private static final String DOCKER_BUILD = "build_docker_image.sh"
+    private static final String DOCKERFILE = "Dockerfile"
 
     @TaskAction
     void run() {
-        final InputStream scriptContent = getClass().getResourceAsStream("build_docker_image.sh")
-        (scriptContent.text).execute()
+        final Path buildDirPath = getProject().getBuildDir().toPath()
+        print(buildDirPath)
+        try {
+            Files.write(buildDirPath.resolve(DOCKER_BUILD), getFileContent(DOCKER_BUILD))
+            Files.write(buildDirPath.resolve(DOCKERFILE), getFileContent(DOCKERFILE))
+        } catch (IOException e) {
+            throw new HandleFileException(e);
+        }
+    }
+
+    private byte[] getFileContent(String resourceName) {
+        final InputStream is = getClass().getResourceAsStream(resourceName)
+        return is.getBytes()
+    }
+
+    private static final class HandleFileException extends RuntimeException {
+
+        HandleFileException(Exception exception) {
+            super(exception)
+        }
     }
 }
--- buildSrc/src/main/resources/build_docker_image.sh
+++ buildSrc/src/main/resources/build_docker_image.sh
@@ -1,1 +1,3 @@
-docker build -t kolegran/sample:latest .
+#!/bin/bash -vx
+
+docker build -t {{DOCKER_REPOSITORY}}/{{UPLOAD_PROJECT_NAME}}:latest
