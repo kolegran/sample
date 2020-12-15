886ed03eb2d7643c2e778dee17f34093d86a5528
1qw <evgen.ischenko+1@gmail.com>
2020-12-15 02:09:31 +0200

    Upgraded testcontainers version, added docker build script modifying

--- buildSrc/src/main/groovy/DockerImageBuildTask.groovy
+++ buildSrc/src/main/groovy/DockerImageBuildTask.groovy
@@ -5,24 +5,30 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 class DockerImageBuildTask extends DefaultTask {
-    private static final String DOCKER_BUILD = "build_docker_image.sh"
+
+    private static final String DOCKER_REPOSITORY = "kolegran"
+    private static final String DOCKER_BUILD_SCRIPT = "build_docker_image.sh"
     private static final String DOCKERFILE = "Dockerfile"
 
     @TaskAction
     void run() {
-        final Path buildDirPath = getProject().getBuildDir().toPath()
-        print(buildDirPath)
         try {
-            Files.write(buildDirPath.resolve(DOCKER_BUILD), getFileContent(DOCKER_BUILD))
-            Files.write(buildDirPath.resolve(DOCKERFILE), getFileContent(DOCKERFILE))
+            modifyDockerBuildScript(DOCKER_BUILD_SCRIPT);
         } catch (IOException e) {
-            throw new HandleFileException(e);
+            throw new HandleFileException(e)
         }
     }
 
-    private byte[] getFileContent(String resourceName) {
-        final InputStream is = getClass().getResourceAsStream(resourceName)
-        return is.getBytes()
+    private void modifyDockerBuildScript(String resourceName) {
+        final InputStream inputStream = getClass().getResourceAsStream(resourceName)
+        final String replaceDockerRepository = inputStream.getText().replace("{{DOCKER_REPOSITORY}}", DOCKER_REPOSITORY)
+        final String replaceUploadProjectName = replaceDockerRepository.replace("{{UPLOAD_PROJECT_NAME}}", getProject().getName())
+        createModifiedFile(DOCKER_BUILD_SCRIPT, replaceUploadProjectName.getBytes())
+    }
+
+    private void createModifiedFile(String resourceName, byte[] content) {
+        final Path buildDirPath = getProject().getBuildDir().toPath()
+        Files.write(buildDirPath.resolve(resourceName), content)
     }
 
     private static final class HandleFileException extends RuntimeException {
